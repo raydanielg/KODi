807970cf002a30eddbb726af5c4637db07272152
raydanielg <airezra2+1@gmail.com>
2026-06-05 09:40:24 +0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -398,6 +398,19 @@
                     }, 1500);
                 });
             }
+
+            // Register Service Worker for PWA
+            if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                    navigator.serviceWorker.register('/service-worker.js')
+                        .then(function(registration) {
+                            console.log('Service Worker registered with scope:', registration.scope);
+                        })
+                        .catch(function(error) {
+                            console.log('Service Worker registration failed:', error);
+                        });
+                });
+            }
         });
     </script>
 
