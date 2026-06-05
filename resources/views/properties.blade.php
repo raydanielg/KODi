942318fb62c859e9478d90d8b8fc987cabb96b3b
raydanielg <airezra2+1@gmail.com>
2026-06-05 10:40:26 +0300

    commit

--- resources/views/properties.blade.php
+++ resources/views/properties.blade.php
@@ -738,7 +738,12 @@
 
             .property-image {
                 height: 180px;
-                font-size: 3rem;
+            }
+
+            .property-image img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
             }
 
             .property-title {
@@ -1123,7 +1128,7 @@
 
             <div class="property-card" data-category="apartments" onclick="window.location.href='/properties/6'">
                 <div class="property-image">
-                    <span>🏢</span>
+                    <img src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?w=800&h=600&fit=crop" alt="City Center Loft">
                     <div class="property-favorite" onclick="event.stopPropagation(); this.classList.toggle('active')">
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
