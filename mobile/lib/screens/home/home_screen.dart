9eefb8d0b53f49e74d0ed5b9eaa878174427c3ab
raydanielg <airezra2+1@gmail.com>
2026-06-05 11:33:59 +0300

    commit

--- mobile/lib/screens/home/home_screen.dart
+++ mobile/lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../constants/app_assets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
