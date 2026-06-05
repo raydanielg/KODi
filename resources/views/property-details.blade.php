949ec98409505b2b977fdc3ef371ad1294ebde7e
raydanielg <airezra2+1@gmail.com>
2026-06-05 22:34:01 +0300

    commit

--- resources/views/property-details.blade.php
+++ resources/views/property-details.blade.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('icons8-logo-16.png') }}">
-    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('icons8-logo-50.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('icons8-logo-50.png') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logo.svg') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
