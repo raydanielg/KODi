8f8cd4e358831e1440fcb4c6f9203edc11448961
raydanielg <airezra2+1@gmail.com>
2026-06-05 21:55:16 +0300

    commit

--- public/index.php
+++ public/index.php
@@ -2,6 +2,18 @@
 
 define('LARAVEL_START', microtime(true));
 
+// Set default timezone
+date_default_timezone_set('Africa/Dar_es_Salaam');
+
+// Error reporting (disable in production)
+if (env('APP_ENV') === 'production') {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+} else {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
