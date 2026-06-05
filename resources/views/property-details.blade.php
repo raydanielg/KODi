56e91668a5ed7de1fdd812934b291b72ade302af
raydanielg <airezra2+1@gmail.com>
2026-06-05 10:49:48 +0300

    commit

--- resources/views/property-details.blade.php
+++ resources/views/property-details.blade.php
@@ -503,13 +503,7 @@
     <nav class="header">
         <div class="nav-container">
             <a href="{{ url('/') }}" class="nav-brand">
-                <div class="nav-brand-icon">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2L2 7L12 12L22 7L12 2Z"/>
-                        <path d="M2 17L12 22L22 17" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-                        <path d="M2 12L12 17L22 12" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-                    </svg>
-                </div>
+                <img src="{{ asset('icons8-logo-50.png') }}" alt="Manna" class="nav-brand-icon">
                 <span class="nav-brand-name">{{ config('app.name', 'Manna') }}</span>
             </a>
             <a href="{{ route('properties') }}" class="nav-back">
