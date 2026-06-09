8a90577807a6a5308fb9cc903f2fcc1613ddc4e9
raydanielg <airezra2+1@gmail.com>
2026-06-09 16:29:05 +0300

    commit

---
+++ app/Http/Controllers/Admin/MaintenanceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaintenanceController extends Controller
+{
+    public function index()
+    {
+        return view('admin.maintenance.index');
+    }
+
+    public function show($id)
+    {
+        return view('admin.maintenance.show', compact('id'));
+    }
+
+    public function edit($id)
+    {
+        return view('admin.maintenance.edit', compact('id'));
+    }
+}
