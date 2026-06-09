6da34252a21d478b1c631e6557e701bb2400f366
raydanielg <airezra2+1@gmail.com>
2026-06-09 18:16:17 +0300

    commit

--- mobile/lib/models/dashboard_stats_model.dart
+++ mobile/lib/models/dashboard_stats_model.dart
@@ -1,13 +1,22 @@
 class DashboardStatsModel {
   final Map<String, dynamic> stats;
-  final List<dynamic>? recentItems;
+  final Map<String, dynamic>? activeLease;
+  final List<dynamic>? recentPayments;
+  final List<dynamic>? maintenanceRequests;
 
-  DashboardStatsModel({required this.stats, this.recentItems});
+  DashboardStatsModel({
+    required this.stats,
+    this.activeLease,
+    this.recentPayments,
+    this.maintenanceRequests,
+  });
 
   factory DashboardStatsModel.fromJson(Map<String, dynamic> json) {
     return DashboardStatsModel(
       stats: Map<String, dynamic>.from(json['stats'] ?? {}),
-      recentItems: json['recent_items'] as List<dynamic>?,
+      activeLease: json['active_lease'] as Map<String, dynamic>?,
+      recentPayments: json['recent_payments'] as List<dynamic>?,
+      maintenanceRequests: json['maintenance_requests'] as List<dynamic>?,
     );
   }
 }
