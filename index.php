9001a865bec7bd943627674f66b60d82d15a0ea2
raydanielg <airezra2+1@gmail.com>
2026-06-09 17:59:50 +0300

    commit

--- index.php
+++ index.php
@@ -1,20 +1,57 @@
 <?php
 
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+/*
+|--------------------------------------------------------------------------
+| Check For Maintenance Mode
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance mode via the "down" command,
+| we will load this view so the user is informed of the application's
+| status. We will simply exit the application since no routes should
+| be available in this state.
+|
+*/
+
 if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll simply require
+| it into the script here so we don't need to manually load any of
+| our classes later on.
+|
+*/
+
 require __DIR__.'/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-$app = require_once __DIR__.'/bootstrap/app.php';
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
-// Set the public path
-$app->usePublicPath(__DIR__.'/public');
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
