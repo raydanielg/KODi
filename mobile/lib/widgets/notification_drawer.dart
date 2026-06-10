ad2612c7b49c35104022c6188a2e7c52e81393ee
raydanielg <airezra2+1@gmail.com>
2026-06-10 15:28:56 +0300

    commit

--- mobile/lib/widgets/notification_drawer.dart
+++ mobile/lib/widgets/notification_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../constants/app_colors.dart';
-import '../models/notification_model.dart';
 import '../services/notification_service.dart';
 import '../utils/helpers.dart';
 
